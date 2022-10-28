cc176b8e5da01f53c1d4637c72a3e97bf97c9aec
MrR341-GER <klopapierklopapier@googlemail.com>
2022-10-28 02:22:46 +0200

    init git
    Signed-off-by: MrR341-GER <klopapierklopapier@googlemail.com>

---
+++ puzzle.js
@@ -0,0 +1,95 @@
+class SVG {
+    constructor(row, col, strokeWidth, imagePath) {
+        this.row = row;
+        this.col = col;
+        this.strokeWidth = strokeWidth;
+        this.image = new Image();
+        this.image.src = imagePath;
+        this.offsetX = 0;
+        this.offsetY = 0;
+        this.movingPuzzle = null;
+        this.svg = null;
+        this.image.onload = () => {
+            this.svg = this.createSVG();
+            document.body.appendChild(svgVar.svg);
+            document.onmouseup = function (e) {
+                svgVar.movingPuzzle = null;
+            };
+        }
+    }
+
+    calcSize() {
+
+    }
+
+    createSVG() {
+        if (this.row == 0 || this.col == 0) {
+            if (this.row == 0 && this.col == 0) {
+                this.col = 10;
+                this.row = 10;
+                var greater = this.row;
+            } else {
+                var greater = this.col < this.row ? this.row : this.col;
+            }
+            if (this.image.width < this.image.height) {
+                this.col = Math.round(this.image.width / (this.image.height / greater));
+                this.row = greater
+            } else {
+                this.row = Math.round(this.image.height / (this.image.width / greater));
+                this.col = greater;
+            }
+            console.log(this.row);
+            console.log(this.col);
+        }
+        const NS = 'http://www.w3.org/2000/svg';
+        let svg = document.createElementNS(NS, 'svg');
+        svg.setAttribute('xmlns', NS);
+        svg.setAttribute('viewBox', `${-this.strokeWidth} ${-this.strokeWidth} ${this.col + this.strokeWidth * 2} ${this.row + this.strokeWidth * 2}`);
+        var style = document.createElementNS(NS, 'style');
+        style.innerHTML = `path{stroke:grey;}`;
+        svg.appendChild(style);
+        let defs = document.createElementNS(NS, "defs");
+        defs.innerHTML = `
+        <pattern id="img1" patternUnits="userSpaceOnUse" width="${this.col}" height="${this.row}">
+            <image href="${this.image.src}" x="0" y="0"  width="${this.col}" height="${this.row}"></image>
+        </pattern>`;
+        svg.appendChild(defs);
+        for (let y = 0; y < this.row; y++) {
+            for (let x = 0; x < this.col; x++) {
+                let path = document.createElementNS(NS, 'path');
+                path.setAttribute("d", `M ${x} ${y} l 1 0 l 0 1 l -1 0 z`);
+                path.setAttribute("stroke-width", this.strokeWidth);
+                path.setAttribute("fill", "url(#img1)");
+                path.offsetX = 0;
+                path.offsetY = 0;
+                path.onmousedown = (e) => {
+                    this.movingPuzzle = path;
+                    let tile = path.getBoundingClientRect();
+                    let { x, y } = this.svg.getBoundingClientRect();
+                    this.offsetX = ((e.clientX - x) / tile["width"]) + this.strokeWidth - path.offsetX;
+                    this.offsetY = ((e.clientY - y) / tile["height"]) + this.strokeWidth - path.offsetY;
+                    svg.appendChild(path);
+                };
+                svg.appendChild(path);
+            }
+        }
+        return svg;
+    }
+}
+
+//let svgVar = new SVG(31, 0, 0.005, './carinanebel.png');
+let svgVar = new SVG(5, 0, 0.005, './stephans-quintett.jpg');
+
+document.onmousemove = (e) => {
+    if (svgVar.movingPuzzle) {
+        let tile = svgVar.movingPuzzle.getBoundingClientRect();
+        let { x, y, width, height } = svgVar.svg.getBoundingClientRect();
+        let newX = ((e.clientX - x) / tile["width"]) - svgVar.offsetX;
+        let newY = ((e.clientY - y) / tile["height"]) - svgVar.offsetY;
+        svgVar.movingPuzzle.setAttribute("transform", `translate(${newX} ${newY})`);
+        svgVar.movingPuzzle.offsetX = newX
+        svgVar.movingPuzzle.offsetY = newY
+    }
+}
+
+
